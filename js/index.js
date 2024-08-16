7cdb38d43d12baf8ddef93399edaade83521b4b6
SumayaaAden <146494071+SumayaaAden@users.noreply.github.com>
2024-08-15 23:04:41 -0600

    Assignment03 Complete!

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".menu-icon");
+const sidebar = document.querySelector(".sidebar");
+
+menuIcon.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  menuIcon.classList.toggle("active");
+});
